dc0da50645100d98582401107872c8c1827a4dce
shrutinimai <nimaishruti@gmail.com>
2025-03-26 12:43:01 +0530

    login page is added and updated

--- userRoutes.js
+++ userRoutes.js
@@ -21,4 +21,25 @@ router.post("/signup", async (req, res) => {
     }
 });
 
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ where: { email } });
+
+        if (!user) {
+            return res.status(404).json({ message: "User doesn't exist" });
+        }
+
+        if (user.password !== password) {
+            return res.status(401).json({ message: "Incorrect password" });
+        }
+
+        res.status(200).json({ message: "Login successful", user });
+
+    } catch (error) {
+        res.status(500).json({ message: "Error logging in", error: error.message });
+    }
+});
+
 module.exports = router;
